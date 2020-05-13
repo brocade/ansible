533b6bdae5ebb0ce5dd0a1a4881654316ddcd58d
Simon Hill <simon.hill@atos.net>
2020-05-13 09:58:58 +0100

    Prevent logging of credentials

--- library/brocade_fibrechannel_switch.py
+++ library/brocade_fibrechannel_switch.py
@@ -132,7 +132,7 @@ def main():
     """
 
     argument_spec = dict(
-        credential=dict(required=True, type='dict'),
+        credential=dict(required=True, type='dict', no_log=True),
         vfid=dict(required=False, type='int'),
         throttle=dict(required=False, type='float'),
         switch=dict(required=True, type='dict'))
--- library/brocade_security_ipfilter_rule.py
+++ library/brocade_security_ipfilter_rule.py
@@ -208,7 +208,7 @@ def main():
     """
 
     argument_spec = dict(
-        credential=dict(required=True, type='dict'),
+        credential=dict(required=True, type='dict', no_log=True),
         vfid=dict(required=False, type='int'),
         throttle=dict(required=False, type='float'),
         ipfilter_rules=dict(required=True, type='list'))
